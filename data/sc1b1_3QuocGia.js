0b23d4ed27877490231ab6a865570ce80b1728ff
khanhn29 <54073251+khanhn29@users.noreply.github.com>
2025-07-26 15:44:02 +0700

    update

---
+++ data/sc1b1_3QuocGia.js
@@ -0,0 +1,18 @@
+const vocabulary = [
+    { "vi": "Hàn Quốc", "han": "한국" },
+    { "vi": "Mỹ", "han": "미국" },
+    { "vi": "Mông Cổ", "han": "몽골" },
+    { "vi": "Anh", "han": "영국" },
+    { "vi": "Việt Nam", "han": "베트남" },
+    { "vi": "Trung Quốc", "han": "중국" },
+    { "vi": "Indonesia", "han": "인도네시아" },
+    { "vi": "Đức", "han": "독일" },
+    { "vi": "Malaysia", "han": "말레이시아" },
+    { "vi": "Thái Lan", "han": "태국" },
+    { "vi": "Philipin", "han": "필리핀" },
+    { "vi": "Pháp", "han": "프랑스" },
+    { "vi": "Nhật", "han": "일본" },
+    { "vi": "Úc", "han": "호주" },
+    { "vi": "Nga", "han": "러시아" },
+    { "vi": "Ấn Độ", "han": "인도" }
+];
