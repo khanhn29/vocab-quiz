c5dc6aa2762de358e09460aa7d93692011439d57
khanhn29 <54073251+khanhn29@users.noreply.github.com>
2025-07-26 15:38:40 +0700

    update

---
+++ data/sc1b1_1NgheNghiep.js
@@ -0,0 +1,12 @@
+const vocabulary = [
+    { "vi": "Học sinh", "han": "학생" },
+    { "vi": "Bác sĩ", "han": "의사" },
+    { "vi": "Dược sĩ", "han": "약사" },
+    { "vi": "Nhân viên công ty", "han": "회사원" },
+    { "vi": "Công chức", "han": "공무원" },
+    { "vi": "Lái xe", "han": "운전기사" },
+    { "vi": "Hướng dẫn viên du lịch", "han": "관광가이드/여행 안내원" },
+    { "vi": "Nội trợ", "han": "주부" },
+    { "vi": "Giáo viên", "han": "교사" },
+    { "vi": "Nhân viên ngân hàng", "han": "은행원" }
+];
