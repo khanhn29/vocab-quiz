67b7f7fe1f8e9f5b92bce5d3425e504d7e104740
khanhn29 <54073251+khanhn29@users.noreply.github.com>
2025-07-26 15:06:30 +0700

    update

---
+++ data/sc1b2_3DoVatTrongPhongHoc.js
@@ -0,0 +1,17 @@
+const vocabulary = [
+    { "vi": "cái bàn", "han": "책상" },
+    { "vi": "cửa sổ", "han": "창문" },
+    { "vi": "bản đồ", "han": "지도" },
+    { "vi": "cái ghế", "han": "의자" },
+    { "vi": "vở", "han": "공책" },
+    { "vi": "bảng", "han": "칠판" },
+    { "vi": "cặp sách", "han": "가방" },
+    { "vi": "từ điển", "han": "사전" },
+    { "vi": "hộp bút", "han": "필통" },
+    { "vi": "máy vi tính", "han": "컴퓨터" },
+    { "vi": "tẩy/ cục tẩy", "han": "지우개" },
+    { "vi": "bút, viết", "han": "펜" },
+    { "vi": "sách", "han": "책" },
+    { "vi": "đồng hồ", "han": "시계" },
+    { "vi": "cửa ra vào", "han": "문" }
+];
